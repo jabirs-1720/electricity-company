0fd2ac2184e7e8ee3705edcfdbd3c9a64a3b6f04
jaber's <jabirs1720@gmail.com>
2025-09-17 14:51:53 +0300

    Bug fix default permissions

--- backend/settings.py
+++ backend/settings.py
@@ -126,8 +126,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated'
-    )
+        'rest_framework.permissions.IsAuthenticated',
+    ),
 }
 
 CSRF_SECURE = os.getenv('CSRF_SECURE', 'False') == 'True'
